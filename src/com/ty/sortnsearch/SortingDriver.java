fb97145fb822fdf477ac248bf501de3bd399c028
Manoj Kumar G U <manojkumargu3@gmail.com>
2022-11-07 14:56:13 +0530

    recent added

--- src/com/ty/sortnsearch/SortingDriver.java
+++ src/com/ty/sortnsearch/SortingDriver.java
@@ -1,11 +1,64 @@
 package com.ty.sortnsearch;
 
-public class SortingDriver {
-	static int[] arr = {9,3,-1,-8};
-	public static void main(String[] args) {
-		Sorting.bubbleSort(arr);
-		for(int value : arr) {
-			System.out.print(value);
-		}
-	}
+import java.io.*;
+
+public class SortingDriver
+
+{
+
+    public static void main(String args[]) throws IOException
+    {
+        String name[] = new String[20];
+        String s, t;
+        int i,f=0,j;
+        int low, high, mid, pos;
+        InputStreamReader in=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(in);
+        System.out.println("Enter 10 Names in array:");
+        for(i=0;i<10;i++)
+        {
+        	name[i] = br.readLine();
+        }
+        System.out.println("Enter name to Search:");
+        s = br.readLine();
+        for(i=0;i<9;i++)
+        {
+            for(j=0;j<9-i;j++)
+            {
+                if(name[j].compareTo(name[j+1])>0)
+                {
+                    t = name[j];
+                    name[j] = name[j+1];
+                    name[j+1] = t;
+                }
+            }
+        }
+        System.out.println ("Sorted array :");
+        for(i=0;i<10;i++)
+        {
+            System.out.print (name[i] + " ");
+        }
+        high = 10;
+        low = 0;
+        pos = 0;
+        while(low <= high)
+        {
+            mid = (low + high) / 2;
+            if(s.compareTo(name[mid])<0)
+            {
+                high = mid - 1;
+            }
+            else if(s.compareTo(name[mid])>0)
+            {
+                low = mid + 1;
+            }
+            else if(s.compareTo(name[mid])==0)
+            {
+                pos = mid + 1;
+                break;
+            }
+        }
+        System.out.println("\n Search result");
+        System.out.println(s+" is located at " + pos);
+    }
 }
